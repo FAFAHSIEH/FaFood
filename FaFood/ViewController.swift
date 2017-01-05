05a882f7f87998673defc15a55622a139ce2be6a
Hsieh Karen <karen@Hsiehde-MacBook-Air.local>
2017-01-05 10:19:35 +0800

    Initial Commit

---
+++ FaFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaFood
+//
+//  Created by Hsieh Karen on 2017/1/5.
+//  Copyright © 2017年 Hsieh Karen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
